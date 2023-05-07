c287a5c7ff310a96157b144dbad6f1200aad5b51
Sajjad Hussain <sajjaditprofessional@gmail.com>
2023-05-06 21:08:00 -0700

    Initial commit

---
+++ component/card.tsx
@@ -0,0 +1,13 @@
+//component
+
+
+const Card =()=>{
+    let a = "my first element"
+    return (
+        <div>
+<h3> {a}</h3>
+
+        </div>
+    )
+}
+export default Card
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Card from "../../component/card"
+
+
+export default function Home() {
+  return ( <div>
+  <h3>Sajjad Hussain </h3>
+  
+  <Card/>
+  <Card/>
+  
+  </div>
+  )
+}
