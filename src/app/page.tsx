800d36ccadfd21160acd31a89253d77089075ed6
Sajjad Hussain <sajjaditprofessional@gmail.com>
2023-06-03 22:27:41 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
      <Navbar/>
-      <h1>Hello Sajjad Hussain Meo </h1>
+      <h1>Hello Sajjad Hussain  </h1>
       <Comp />
       
     </div>
